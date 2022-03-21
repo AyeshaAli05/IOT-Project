9757ff64ae0ddcebada2a76727e2c06620ed43ea
Ayesha Ali <76215573+AyeshaAli05@users.noreply.github.com>
2022-03-21 12:59:14 +0700

    Add files via upload

---
+++ Password_key_pad.ino
@@ -0,0 +1,53 @@
+#include <Keypad.h>
+const int ROW_NUM = 4; //four rows
+const int COLUMN_NUM = 3; //three columns
+char keys[ROW_NUM][COLUMN_NUM] = {
+  {'1','2','3'},
+  {'4','5','6'},
+  {'7','8','9'},
+  {'*','0','#'}
+};
+
+byte pin_rows[ROW_NUM] = {13, 12, 14, 27}; //connect to the row pinouts of the keypad
+byte pin_column[COLUMN_NUM] = {33, 25, 26}; //connect to the column pinouts of the keypad
+Keypad keypad = Keypad( makeKeymap(keys), pin_rows, pin_column, ROW_NUM, COLUMN_NUM );
+const String password_1 = "1234"; // change your password here
+const String password_2 = "5642"; // change your password here
+const String password_3 = "4545"; // change your password here
+String input_password;
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(2,OUTPUT);
+  input_password.reserve(32); // maximum input characters is 33, change if needed
+}
+void loop()
+{ 
+char key = keypad.getKey();
+if (key)
+{
+Serial.println(key);
+if(key == '*') 
+{
+input_password = ""; // reset the input password  
+}
+else if(key == '#') 
+{
+if(input_password == password_1 || input_password == password_2 || input_password == password_3) {
+Serial.println("password is correct");
+digitalWrite(2,HIGH);
+delay(2000);
+digitalWrite(2,LOW);
+} 
+else 
+{
+Serial.println("password is incorrect");
+}
+input_password = ""; // reset the input password
+} 
+else 
+{
+input_password += key; // append new character to input password string
+}
+}
+}
